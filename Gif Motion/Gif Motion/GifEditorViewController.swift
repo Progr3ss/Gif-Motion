fdb12b4d9b896349389e3e1cdae18d663f1aab48
martin <mchibwe01@gmail.com>
2016-09-01 23:37:24 -0700

    new

--- Gif Motion/Gif Motion/GifEditorViewController.swift
+++ Gif Motion/Gif Motion/GifEditorViewController.swift
@@ -14,28 +14,18 @@ class GifEditorViewController: UIViewController {
 	
 	@IBOutlet weak var captionTextField: UITextField!
 	@IBOutlet weak var gifImageView: UIImageView!
-//	var gifURL : NSURL? = nil
+
 	var gif: Gif?
 
 	
 	override func viewWillAppear(animated: Bool) {
 		super.viewWillAppear(animated)
 		gifImageView.image = gif?.gifImage
-		
-//		if let gifURL = gifURL{
-//			
-//			let gifFromRecording = UIImage.gifWithURL(String(gifURL))
-////			gifImageView.image = gifFromRecording
-//			gifImageView.image = gif?.gifImage
-//			
-//			
-//		}
+
 	}
     override func viewDidLoad() {
         super.viewDidLoad()
 		
-
-        // Do any additional setup after loading the view.
     }
 
 
